4785008e925a2dcbd449261c12e8146f8a5a0a22
La Mura, Pasquale <pasquale.lamura@nttdata.com>
2022-12-20 15:41:23 +0100

    aggiunto esmepio log

--- src/main/java/com/example/social/controller/UserController.java
+++ src/main/java/com/example/social/controller/UserController.java
@@ -3,12 +3,14 @@ package com.example.social.controller;
 import com.example.social.collection.User;
 import com.example.social.dtos.UserDTO;
 import com.example.social.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
@@ -23,7 +25,9 @@ public class UserController {
 
     @PostMapping(path = "/users")
     public Mono<ResponseEntity<UserDTO>> saveUser(@RequestBody UserDTO userDTO){
-     return userService.save(mapper.map(userDTO, User.class)).map(user -> ResponseEntity.ok(mapper.map(user,UserDTO.class)));
+     return userService.save(mapper.map(userDTO, User.class))
+             .doOnNext(user -> log.info("Utente salvato {}",user))
+             .map(user -> ResponseEntity.ok(mapper.map(user,UserDTO.class)));
     }
 
     @DeleteMapping(path = "/users/{id}")
